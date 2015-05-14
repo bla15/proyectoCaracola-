b082918ac0d0743bf6d61732d920d913be16080d
Alumno <Alumno@10.207.99.110>
2015-05-14 13:49:17 +0200

    Estructura de profesores añadida

---
+++ proyectoCaracola++/src/profesor.h
@@ -0,0 +1,40 @@
+/*
+ * profesor.h
+ *
+ *  Created on: 14/05/2015
+ *      Author: Alumno
+ */
+
+#ifndef PROFESOR_H_
+#define PROFESOR_H_
+
+namespace contenedorProfesor{
+class Profesor{
+	int dni;
+	std::string clave;
+	std::string nombre;
+	std::string apellido;
+	int telefono;
+
+public:
+	Profesor(int dni, std::string clave, std::string nombre, std::string apellido, int telefono);
+	int getDni() const;
+	void setDni(int dni);
+
+	std::string getClave() const;
+	void setClave(std::string clave);
+
+	std::string getNombre() const;
+	void setNombre(std::string nombre);
+
+	std::string getApellido() const;
+	void setApellido(std::string apellido);
+
+	int getTelefono() const;
+	void setTelefono(int telefono);
+
+
+};
+}
+
+#endif /* PROFESOR_H_ */
